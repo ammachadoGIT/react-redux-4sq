dbfafe96ce14a53e01c537f29602cf4d7e0809da
Andre Machado <ammachado@gmx.com>
2018-06-06 18:29:50 -0400

    use ActionTypes

--- src/util/actions.js
+++ src/util/actions.js
@@ -1,26 +1,21 @@
-export const LIST_DATA = "LIST_DATA";
-export const START_LOADING = "START_LOADING";
-export const STOP_LOADING = "STOP_LOADING";
-export const SET_LOCATION = "SET_LOCATION";
-export const SET_ERROR = "ERROR";
-export const SET_FILTER = "SET_FILTER";
+import * as ActionTypes from "../constants/ActionTypes";
 
 export function listData(data) {
-    return { type: LIST_DATA, data };
+    return { type: ActionTypes.LIST_DATA, data };
 }
 
 export function startLoading() {
-    return { type: START_LOADING };
+    return { type: ActionTypes.START_LOADING };
 }
 
 export function stopLoading() {
-    return { type: STOP_LOADING };
+    return { type: ActionTypes.STOP_LOADING };
 }
 
 export function error(error) {
-    return { type: SET_ERROR, error };
+    return { type: ActionTypes.SET_ERROR, error };
 }
 
 export function setFilter(filter) {
-    return { type: SET_FILTER, filter };
+    return { type: ActionTypes.SET_FILTER, filter };
 }
