1cd7fc3ff94a018405e34e90ead7bcda4e83435a
Andre Machado <ammachado@gmx.com>
2018-06-06 01:53:55 -0400

    map state and dispatch to props

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Row, Button, Panel } from 'react-bootstrap';
+import { setFilter } from '../util/actions';
+import { connect } from 'react-redux';
 
-export default class FilterPanel extends Component {
+class FilterPanel extends Component {
 
     constructor() {
         super();
-        this.state = { radius: 2000 };
     }
     setRadius(event) {
-        this.setState({ radius: event.target.value });
+        this.props.setFilter({ radius: event.target.value });
     }
 
     render() {
+        let radius = this.props.filter.radius;
+
         return (
             <Panel className="col-md-12 text-center panel-filter">
                 <Panel.Heading>
@@ -21,23 +24,37 @@ export default class FilterPanel extends Component {
                     <form>
                         <Row>
                             <label htmlFor="radius-slider" >Radius:</label>
-                            {
-                                this.state.radius ?
-                                    <span id="radius-slider-value">{this.state.radius} m</span> : ""
-                            }
+                            <span id="radius-slider-value">{radius} m</span>
                         </Row>
                         <Row className="text-center col-md-8 col-md-offset-2">
                             <input id="radius-slider" type="range" min="500" max="5000" maxLength="5" step="500"
-                                onChange={this.setRadius.bind(this)} defaultValue={this.state.radius} />
+                                onChange={this.setRadius.bind(this)} defaultValue={radius} />
                         </Row>
                     </form>
                 </Panel.Body>
                 <Panel.Footer>
                     <Row>
-                        <Button bsStyle="primary" onClick={this.props.fnSearch.bind(this, this.state.radius)}>Search</Button>
+                        <Button bsStyle="primary" onClick={this.props.fnSearch.bind(this, radius)}>Search</Button>
                     </Row>
                 </Panel.Footer>
             </Panel>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        store: state,
+        filter: state.filter
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setFilter: (filter) => {
+            dispatch(setFilter(filter));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterPanel)
