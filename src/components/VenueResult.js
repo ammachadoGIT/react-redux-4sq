370ba9c15df9af733280196120bab08cfa26b0cf
Andre Machado <ammachado@gmx.com>
2018-06-06 01:54:39 -0400

    add file

---
+++ src/components/VenueResult.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import VenueList from './VenueList';
+import { connect } from 'react-redux';
+
+class VenueResult extends Component {
+  render() {
+    return this.props.isLoadingData ?
+      <img src="../img/loader.gif" alt="" /> :
+      <VenueList />
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoadingData: state.isLoadingData
+  }
+}
+
+
+export default connect(mapStateToProps)(VenueResult)
