44c5ade9c42576bfaa2837976eba22f30c8b803b
Andre Machado <ammachado@gmx.com>
2018-06-06 18:28:45 -0400

    add file

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,6 @@
+export const LIST_DATA = "LIST_DATA";
+export const START_LOADING = "START_LOADING";
+export const STOP_LOADING = "STOP_LOADING";
+export const SET_LOCATION = "SET_LOCATION";
+export const SET_ERROR = "ERROR";
+export const SET_FILTER = "SET_FILTER";
