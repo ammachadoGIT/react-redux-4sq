711bced7ebdf6055926a183561fab4bb916dc51b
Andre Machado <ammachado@gmx.com>
2018-07-03 02:33:12 -0400

    add cache to avoid multiple calls to 4sq api

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const memjs = require("memjs");
 const express = require("express");
 const request = require("request");
 const qs = require("querystring");
@@ -12,12 +13,11 @@ const apiExplore = "https://api.foursquare.com/v2/venues/explore/";
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/api/explore", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+const memcacheSettings = { expires: 30 };
 
-    const ll = req.query.ll;
-    const radius = req.query.radius;
-    const section = req.query.section;
+
+
+const callFoursquareApi = (cacheKey, ll, section, radius, res) => {
 
     const params = {
         client_id: clientId,
@@ -41,9 +41,44 @@ app.get("/api/explore", (req, res) => {
         if (err) {
             res.send(err);
         } else {
+            memjsClient.set(cacheKey, data, memcacheSettings)
             res.send(JSON.parse(data));
         }
     });
+}
+
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+
+var memjsClient = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
+    username: process.env.MEMCACHEDCLOUD_USERNAME,
+    password: process.env.MEMCACHEDCLOUD_PASSWORD
+});
+
+app.get("/api/explore", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    const ll = req.query.ll;
+    const radius = req.query.radius;
+    const section = req.query.section;
+
+    const cacheKey = ll + radius + section;
+
+    memjsClient
+        .get(cacheKey)
+        .then((data) => {
+            if (!data.value) {
+                console.log("not using cache for key", cacheKey)
+                callFoursquareApi(cacheKey, ll, section, radius, res);
+            } else {
+                console.log("using cache for key", cacheKey)
+                res.send(JSON.parse(ab2str(data.value)));
+            }
+        })
+        .catch(err => {
+            console.log("error", err)
+        });
 });
 
 if (process.env.NODE_ENV === 'production') {
