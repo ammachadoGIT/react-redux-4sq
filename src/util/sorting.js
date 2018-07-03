a45491cda1ad84c3443b9edcd46225ebce2299fd
Andre Machado <ammachado@gmx.com>
2018-07-02 23:42:28 -0400

    change function to ES6

--- src/util/sorting.js
+++ src/util/sorting.js
@@ -1,3 +1,2 @@
-export const sortByDistance = function(a,b) {
-    return a.venue.location.distance - b.venue.location.distance;
-};
+export const sortByDistance = (a,b) =>
+    a.venue.location.distance - b.venue.location.distance
